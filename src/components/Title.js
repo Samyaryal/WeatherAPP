c43275550dd187fa3ea35e5790ebbbd4f2f08685
Samikshya <samikshyaaryal5@gmail.com>
2020-02-06 23:23:17 +0200

    chaning class component to functional stateless component

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
-class Title extends React.Component{
-    render() {
-        return (
-            <div>
-               <h1>Weather Finder</h1>
-               <p>Find out Temperature, Condition and more...</p>
-            </div>
-        );
-    }
-};
+const Title = () =>(
+    <div>
+        <h1>Weather Finder</h1>
+        <p>Find out Temperature, Condition and more...</p>
+    </div>
+)
 export default Title;
