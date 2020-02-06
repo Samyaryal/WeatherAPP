4a8d13dd18bd8d5e40c750256258efd42ee670eb
Samikshya <samikshyaaryal5@gmail.com>
2020-02-06 22:12:16 +0200

    success to get data in the console

--- src/App.js
+++ src/App.js
@@ -8,14 +8,17 @@ const API_KEY = "80c2b95addde04faa8ba32f8b33799a2";
 
 
 class App extends React.Component{
-  getWeather = async () => {
-    const api_call = await fetch (``)
+  getWeather = async (e) => {
+    e.preventDefault();
+    const api_call = await fetch (`http://api.openweathermap.org/data/2.5/weather?q=Manchester, uk&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+    console.log(data);
   }
   render () {
     return (
       <div>
         <Title />
-        <Form />
+        <Form getWeather={this.getWeather}/>
         <Weather />
       </div>
     );
